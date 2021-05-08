f90dea443c761b7e9f715e86a6fa6e8bbc16c10d
VCRA\ppattepu <ppattepu@vocera.com>
2021-05-08 16:06:50 +0530

    FireBase App Connection

--- instagram-clone-react/src/App.js
+++ instagram-clone-react/src/App.js
@@ -1,27 +1,32 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Post from './Post'
+import { db } from './firebase'
+
 function App() {
   const [posts, setPosts]= useState([
-  {userName:"Manoj Kiran", caption:"Yo Mam Chill...", imageURL: "https://i.imgur.com/QFbBwXY.jpg"},
-  {userName: "Kullu Manali", caption: "Memories...", imageURL: "https://i.imgur.com/EMoBy0e.jpg"},
-  {userName: "Totti Fellow", caption: "totti ga", imageURL: "https://i.imgur.com/8YpJp3C.jpg"}
+
   ]);
 
-
+  useEffect(()=> {
+    db.collection('posts').onSnapshot(snapshot =>{
+      setPosts(snapshot.docs.map(doc  => ({
+        id: doc.id,
+        post: doc.data()
+      })));
+    })
+  },[]); 
 
   return (
     <div className="app">
-      {/* <h1>Instagram-Clone</h1> */}
-
       <div className="app__header">
       <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt=""/>
       </div>
       <h3> InstaGram </h3>
 
       {
-        posts.map(post => (
-          <Post userName={post.userName} caption={post.caption} imageURL={post.imageURL}/>
+        posts.map(({id, post}) => (
+          <Post key={id} username={post.username} caption={post.caption} imageURL={post.imageURL}/>
         ))
       }
 
--- instagram-clone-react/src/Post.js
+++ instagram-clone-react/src/Post.js
@@ -2,16 +2,16 @@ import React from 'react'
 import './Post.css'
 import Avatar from "@material-ui/core/Avatar"
 
-function Post({userName, caption, imageURL}) {
+function Post({username, caption, imageURL}) {
     return (
         <div className="post">
             <div className="post__header">
                 <Avatar className="post__avatar" alt= "Name" src="/static/images/avatar/1.jpg"/>
-                <h3>{userName}</h3>
+                <h3>{username}</h3>
             </div>
 
             <img class="post_image" src={imageURL} alt="" />
-            <h4 className="post__text"><b>{userName}</b>  {caption} </h4> 
+            <h4 className="post__text"><b>{username}</b>  {caption} </h4> 
         </div>
     )
 }
